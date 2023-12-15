23887b51ce01de1f71e9a49d1f231c6e8e3fbb0b
bikram-choudhury <bikram.choudhury527@gmail.com>
2023-12-15 19:36:41 +0530

    reauthentication using interceptor

--- src/http/interceptor.js
+++ src/http/interceptor.js
@@ -5,13 +5,13 @@ export function initInterceptors() {
     requestInterceptor();
     responseInterceptor();
 }
-
+// helpful doc: https://lightrains.com/blogs/axios-intercepetors-react/
 function requestInterceptor() {
     axios.interceptors.request.use(request => {
-        const headers = getHeaders(request.method);
-        const API_KEY = getAPIKey();
-        // headers['X-RapidAPI-Key'] = API_KEY;
-        request.headers = headers;
+        if(!request.headers.has('X-RapidAPI-Host')) {
+            const headers = getHeaders(request.method);
+            request.headers = headers;
+        }
         // throw new Error('error');
         return request;
     });
@@ -21,20 +21,17 @@ function responseInterceptor() {
     axios.interceptors.response.use(
         response => response,
         error => {
-            const customErr = {
-                statusCode: error.response.status,
-                code: error.code,
-                message: error.message
+            console.log(error);
+            const prevReq = error.config;
+            if(error.code === "ERR_BAD_REQUEST" && !prevReq.retry) {
+                prevReq.retry = true;
+                const prevHeaders = prevReq.headers;
+                const API_KEY = getAPIKey();
+                prevHeaders['X-RapidAPI-Key'] = API_KEY;
+                
+                return axios(prevReq);
             }
-            return Promise.reject(customErr);
-            /* // If client does handle only success response
-            return Promise.resolve({
-                data: {
-                    data: {
-                        languages: []
-                    }
-                }
-            }) */
+            return error;
         }
     )
 }
